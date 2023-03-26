45178d40a506dfa8d942da9641b639db39c867e3
Marcos Vinicius dos Reis Alves <75844594+Marcos1110@users.noreply.github.com>
2023-03-25 21:01:00 -0300

    Delete ex003.py

--- ex003.py
+++
@@ -1,4 +0,0 @@
-valor1 = int(input('Digite um número : '))
-valor2 = int(input('Digite outro : '))
-soma = valor1 + valor2
-print('A soma de [{}] e [{}] é igual a [{}]'.format(valor1, valor2, soma))
